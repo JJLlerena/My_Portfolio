1609b37f0e17cb2d4e7ca7720dd5c1dd6e022d00
Johnny L <Johnnyjllerena@gmail.com>
2023-03-07 02:30:52 -0500

    clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './Navigation/Navbar';
 
 function App() {
   return (
-    <p>
-      Cleanedup React App
-    </p>
+    <div>
+      <Header/>
+      <h1>
+        Johnny Llerena Resume
+      </h1>
+    </div>
   );
 }
 
