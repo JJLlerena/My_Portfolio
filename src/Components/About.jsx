6b67f9518576147f9a4a12500ef4d7eca558c293
Johnny L <Johnnyjllerena@gmail.com>
2023-10-27 05:32:03 -0400

    Modifying original

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,9 +1,8 @@
 import { Container } from 'react-bootstrap';
-import styles from '../styles.module.css'
 
 function About() {
     return(
-        <Container className={styles['component-body']}>
+        <Container>
          
         </Container>
     );
